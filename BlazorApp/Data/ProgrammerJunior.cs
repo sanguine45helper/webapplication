f63328003a652836aac21db1886b0ae61f0e2a81
sanguine45helper <95624478+sanguine45helper@users.noreply.github.com>
2021-12-07 20:15:48 +0200

    remove some whitespace
    remove an extra whitespace

--- BlazorApp/Data/ProgrammerJunior.cs
+++ BlazorApp/Data/ProgrammerJunior.cs
@@ -2,7 +2,7 @@ namespace BlazorApp.Data;
 
 public class ProgrammerJunior
 {
-	public int ProgrammerJuniorId { get; set; }
+    public int ProgrammerJuniorId { get; set; }
     public string username { get; set; }
     public string password { get; set; }
     public string firstname { get; set; }
